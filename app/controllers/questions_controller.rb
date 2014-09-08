8d8ebe147336253da5d65fe1edb234b3ab0300f5
lee kebler & Diane Douglas <student@epicodus.com>
2014-09-08 14:49:47 -0700

    Add question methods and views for new, edit and index.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,49 @@
 class QuestionsController <ApplicationController
 
   def index
-  end
-
-  def new
     @questions = Question.all
   end
 
-  def create
+  def new
+    @question = Question.new
+  end
 
+  def create
+    @question = Question.new(question_params)
+    @question.user_id = current_user.id
+    if @question.save
+      redirect_to root_path, notice: "Hi there! You asked a question! Good work!"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to root_path, notice: "Hi there! You updated your question!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to root_path, notice: "Question removed."
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:question)
   end
 
 end
---
+++ app/models/question.rb
@@ -0,0 +1,5 @@
+class Question <ActiveRecord::Base
+  # has_many :answers
+  belongs_to :user
+  validates_presence_of :question
+end
