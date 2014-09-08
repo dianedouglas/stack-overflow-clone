0866e54a504bd7cce3e108066a8f551ec3d55a10
lee kebler & Diane Douglas <student@epicodus.com>
2014-09-08 11:50:00 -0700

    Add user model tests.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,1 +1,10 @@
 require 'rails_helper'
+
+
+describe User do
+
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
+  it { should have_secure_password }
+
+end
