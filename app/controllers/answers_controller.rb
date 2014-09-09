ab14e277aed7e1754baae93a64cd9424e8231192
lee kebler & Diane Douglas <student@epicodus.com>
2014-09-09 11:41:09 -0700

    Add error display for saving an answer with ajax.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,28 +2,15 @@ class AnswersController <ApplicationController
 
   before_filter :authorize, only: [:create]
 
-  # def index
-  #   redirect_to root_path
-  # end
-
   def create
     @answer = Answer.new(answer_params)
     @answer.user_id = current_user.id
     @answer.question_id = params[:question_id]
-    if @answer.save
-      flash[:notice] = "Good Answer Bro!"
-      respond_to do |format|
-        # format.html{redirect_to question_path(params[:question_id])}
+    @question = @answer.question
+    respond_to do |format|
         format.js
-      end
-      # redirect_to question_path(params[:question_id])
-    else
-      flash[:notice] = "Fill in an answer before submitting!"
-      @question = @answer.question
-      render "questions/show"
     end
   end
-
 
 private
   def answer_params
