87bd119fe8f18cd301f54c2f76c3dc797a14f992
lee kebler & Diane Douglas <student@epicodus.com>
2014-09-09 11:30:16 -0700

    add AJAX for the creation of answers to questions.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,17 +2,25 @@ class AnswersController <ApplicationController
 
   before_filter :authorize, only: [:create]
 
+  # def index
+  #   redirect_to root_path
+  # end
+
   def create
     @answer = Answer.new(answer_params)
     @answer.user_id = current_user.id
     @answer.question_id = params[:question_id]
     if @answer.save
-      redirect_to question_path(params[:question_id])
       flash[:notice] = "Good Answer Bro!"
+      respond_to do |format|
+        # format.html{redirect_to question_path(params[:question_id])}
+        format.js
+      end
+      # redirect_to question_path(params[:question_id])
     else
-      redirect_to question_path(params[:question_id])
       flash[:notice] = "Fill in an answer before submitting!"
-
+      @question = @answer.question
+      render "questions/show"
     end
   end
 
